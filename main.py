d3c508ee728d512e09cf600eb35bca1920c8fc99
HandyWork1 <khalilbrainiarc1@gmail.com>
2023-11-04 21:16:59 +0300

    Prime number generator function

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import random
+import math
 
+def p_numbers(a, b):
+    prime_list = []
+    for i in range(max(2, a), b + 1):
+        is_prime = True
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_list.append(i)
 
 def main():
     # Taking parameters for selecting prime numbers
