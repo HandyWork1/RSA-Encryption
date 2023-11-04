29ece40b9a1b64a21ae1cd57a86cd07e090329ba
HandyWork1 <khalilbrainiarc1@gmail.com>
2023-11-04 23:38:40 +0300

    Test prime number generator function

--- test_main.py
+++ test_main.py
@@ -9,6 +9,18 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(phi(10), 4)  # phi(10) should return 4
         self.assertEqual(phi(13), 12)  # phi(13) should return 12
 
+    def test_p_numbers_function(self):
+        # Test case for p_numbers() function
+        n, prime_list_1_10 = p_numbers(1, 10)
+        self.assertEqual(len(prime_list_1_10), 4)  # There are 4 prime numbers in the range [1, 10]
+
+        n, prime_list_20_30 = p_numbers(20, 30)
+        self.assertEqual(len(prime_list_20_30), 2)  # There are 2 prime numbers in the range [20, 30]
+
+        n, prime_list_50_60 = p_numbers(50, 60)
+        self.assertEqual(len(prime_list_50_60), 2)  # There are 2 prime numbers in the range [50, 60]
+
+
 
 if __name__ == '__main__':
     unittest.main()
