82cc843de156c694440cef5c07dd812e6de740fa
HandyWork1 <khalilbrainiarc1@gmail.com>
2023-11-04 21:07:09 +0300

    Taking inputs in the main method

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    # Taking parameters for selecting prime numbers
+    # Taking message as a numerical value for encryption
+    a = int(input("Enter lower bound of the interval: "))
+    b = int(input("Enter upper bound of the interval: "))
+    msg = int(input("Enter message for encryption: "))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
