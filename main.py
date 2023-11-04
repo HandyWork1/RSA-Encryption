46d2387cd1cb491dd9194b92d5fffeb40fffe2f1
iankariuki <91960341+iankariuki@users.noreply.github.com>
2023-11-04 21:36:12 +0300

    Update main.py
    eulers totient fucntion has been add to the code

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-
+# A simple method to evaluate Euler's Totient Function
+def phi(n):
+    result = 1
+    for i in range(2, n):
+        if math.gcd(i, n) == 1:
+            result += 1
+    return result
 
 def main():
     # Taking parameters for selecting prime numbers
